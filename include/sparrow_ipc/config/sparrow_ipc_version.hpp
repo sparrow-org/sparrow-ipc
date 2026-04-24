651642e21634c1b1c6e88b3752dbd8a076cf7205
GitHub Action <runner@runnervmeorf1.0jkgfptf2jyebe3d5a3js42muf.cx.internal.cloudapp.net>
2026-04-24 14:48:13 +0000

    Release version 1.0.2

--- include/sparrow_ipc/config/sparrow_ipc_version.hpp
+++ include/sparrow_ipc/config/sparrow_ipc_version.hpp
@@ -4,13 +4,13 @@ namespace sparrow_ipc
 {
     constexpr int SPARROW_IPC_VERSION_MAJOR = 1;
     constexpr int SPARROW_IPC_VERSION_MINOR = 0;
-    constexpr int SPARROW_IPC_VERSION_PATCH = 1;
+    constexpr int SPARROW_IPC_VERSION_PATCH = 2;
 
     // Binary version
     // See the following URL for explanations about the binary versionning
     // https://www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html#Updating-version-info
     constexpr int SPARROW_IPC_BINARY_CURRENT = 4;
-    constexpr int SPARROW_IPC_BINARY_REVISION = 1;
+    constexpr int SPARROW_IPC_BINARY_REVISION = 2;
     constexpr int SPARROW_IPC_BINARY_AGE = 0;
 
     static_assert(SPARROW_IPC_BINARY_AGE <= SPARROW_IPC_BINARY_CURRENT, "SPARROW_IPC_BINARY_AGE cannot be greater than SPARROW_IPC_BINARY_CURRENT");
